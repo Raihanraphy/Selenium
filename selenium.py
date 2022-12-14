8713a143a05a3d56f9ba0e50ca5d6f3b0e5db7ba
Raihan Rahman <raihanraphy@gmail.com>
2022-12-14 16:00:30 +0600

    Create selenium.py

---
+++ selenium.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import os 
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+driver.implicitly_wait(0.5)
+driver.maximize_window()
+
+driver.get("https://www.ilovepdf.com/pdf_to_word")
+time.sleep(5)
+s = driver.find_element(By.ID,'pickfiles').send_keys("C:\\Users\\TigerIT\\Downloads\\SQA.pdf")
+#file path specified with send_keys
+time.sleep(200)
